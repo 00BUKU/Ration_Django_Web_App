a59f5158e99620906e03609e6e3794d3b334893b
mmorganj41 <mmorganj41@gmail.com>
2023-01-05 15:59:35 -0500

    recipe details view and url create

--- ration/urls.py
+++ ration/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'), #**kwargs, key word argument
     path('about/', views.about, name='about'),
     path('recipes/', views.recipes_index, name='index'),
-    # path('recipes/<int:recipes_id>/', views.recipes_detail, name="detail"),
+    path('recipes/<int:recipe_id>/', views.recipes_detail, name="detail"),
     path('accounts/signup', views.signup, name='signup')
 ]
--- ration/views.py
+++ ration/views.py
@@ -19,6 +19,10 @@ def recipes_index(request):
   recipes = Recipe.objects.all()
   return render(request, 'recipes/index.html', {'recipes': recipes})
 
+def recipes_detail(request, recipe_id):
+  recipe = Recipe.objects.get(id=recipe_id)
+  return render(request, 'recipes/detail.html', {'recipe': recipe})
+
 def signup(request):
   error_message = ''
   if request.method == 'POST':
