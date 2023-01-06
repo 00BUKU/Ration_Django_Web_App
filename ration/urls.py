9f93fed7b79d1a1438659ffa58e6bc99fb5b4df2
00buku <clydescottwilliamsjr@gmail.com>
2023-01-06 14:12:00 -0600

    recipe form and class based view for recipe

--- ration/urls.py
+++ ration/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('recipes/', views.recipes_index, name='index'),
     path('recipes/<int:recipe_id>/', views.recipes_detail, name="detail"),
-    path('accounts/signup', views.signup, name='signup')
+    path('accounts/signup', views.signup, name='signup'),
+    path('recipes/create/', views.RecipeCreate.as_view(), name='recipe_create'),
 ]
